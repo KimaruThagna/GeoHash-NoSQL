ccc843902bf3301a82b7ed4324cdb0ef8669d24b
Kimaru thagana <macbuntu@localhost.localdomain>
2019-07-24 10:39:44 +0300

    add put to db function

--- add_retrieve_db.py
+++ add_retrieve_db.py
@@ -5,11 +5,19 @@ dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="htt
 
 class DynamoDBCommunicator():
     groceries_table=dynamodb.Table('Grocery_stores')
-    def add_to_dynamodb(self):
-        pass
+    def add_to_dynamodb(self,item):
+        grocery_stores.put_item(
+            Item={
+                  "id":item["id"],
+                  "name":item["name"],
+                  "geohash":Geohash.encode(item["location"][0], item["location"][1])
+
+            }
+        )
+        return f'Item {item["name"]} has been added succesfully '
+
     def retrieve_from_dynamodb(self):
         pe = 'id, name, geohash'
-
 
         response = self.groceries_table.scan(
             # what we are retrieving from DB
